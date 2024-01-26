d408e9b7c97770c621c2d9da6b425b798e2c5076
wilaponce <wilaponce@gmail.com>
2024-01-26 00:22:13 -0800

    Add project files.

---
+++ ListExample/Models/Category.cs
@@ -0,0 +1,17 @@
+﻿namespace ListExample.Models
+{
+
+
+    public class Categories
+    {
+        public Category[]? CategoriesList { get; set; }
+    }
+
+    public class Category
+    {
+        public string? Name { get; set; }
+        public Category[] Categories { get; set; } = new Category[0];
+    }
+
+
+}
---
+++ ListExample/Pages/Index.cshtml.cs
@@ -0,0 +1,42 @@
+﻿using ListExample.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.Json;
+
+namespace ListExample.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+        private IWebHostEnvironment _env;
+        public List<Category> CategoryList { get; set; }
+
+        public IndexModel(ILogger<IndexModel> logger, IWebHostEnvironment env)
+        {
+            _logger = logger;            _env = env;
+
+        }
+
+        public void OnGet()
+        {
+
+            CategoryList = Categories().Categories.ToList();
+        }
+    
+        public Category Categories()
+        {
+
+            var path = Path.Combine(_env.WebRootPath    , "siteData/Categories.json");
+            using StreamReader reader = new(path);
+            var lines = reader.ReadToEnd();
+            var categories = JsonSerializer.Deserialize<Category>(lines);
+            
+            return categories;
+
+        }
+    }
+}
